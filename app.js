0438ba28d7b8a82ecf8478edfd8e44d7db3f7679
Jhuy02 <ttruonghuy2502@gmail.com>
2024-06-20 10:03:47 +0700

    huy

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const reason_popup_item = document.querySelectorAll(".reason_popup_item");
 const modal_elemet = document.querySelector(".modal");
+const close = document.querySelector(".modal_close svg");
+const nav_menu_icon_menu = document.getElementById("nav_menu_icon_menu");
+const nav_menu_icon_X = document.getElementById("nav_menu_icon_X");
 reason_popup_item.forEach((item) => {
   item.addEventListener("click", function () {
-    console.log("lot");
     modal_elemet.style.display = "flex";
   });
 });
@@ -25,4 +27,21 @@ modal_elemet.addEventListener("click", function (event) {
   if (!event.target.closest(".modal_container")) {
     modal_elemet.style.display = "none";
   }
+});
+close.addEventListener("click", function () {
+  modal_elemet.style.display = "none";
+});
+const modal_navmenufixed = document.querySelector(".modal_navmenufixed");
+nav_menu_icon_menu.addEventListener("click", function () {
+  modal_navmenufixed.style.transform = "translateX(0%)";
+  nav_menu_icon_menu.style.display = "none";
+  nav_menu_icon_X.style.display = "block";
+  navbar.style.transform = "translateY(0%)";
+  document.querySelector("body").style.overflow = "hidden";
+});
+nav_menu_icon_X.addEventListener("click", function () {
+  modal_navmenufixed.style.transform = "translateX(100%)";
+  nav_menu_icon_menu.style.display = "block";
+  nav_menu_icon_X.style.display = "none";
+  document.querySelector("body").style.overflow = "auto";
 });
